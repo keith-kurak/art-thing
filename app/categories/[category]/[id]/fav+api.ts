c545a0193c209fcd3d6dc6d796473f8f01fd7560
Keith Kurak <keith.kurak@gmail.com>
2024-01-16 01:23:17 -0500

    failing

--- app/categories/[category]/[id]/fav+api.ts
+++ app/categories/[category]/[id]/fav+api.ts
@@ -1,20 +1,57 @@
 import { ExpoRequest, ExpoResponse } from 'expo-router/server';
+import { Sequelize, DataTypes } from 'sequelize';
+import SQLite from 'sqlite3';
 
-let favs = {};
-console.log('reset')
+const favs = {};
+
+console.log('start')
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './db.sqlite',
+  dialectOptions: {
+    mode: SQLite.OPEN_CREATE
+  }
+});
+
+console.log('end')
+
+const Favs = sequelize.define('Favs', {
+  // Model attributes are defined here
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  favs: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
 
 export async function GET(request: ExpoRequest) {
   const id = request.expoUrl.pathname.split('/')[3];
-  return ExpoResponse.json({ favs: favs[id] || 0 });
+  const favs = await Favs.findAll({
+    where: {
+      id
+    }
+  });
+  return ExpoResponse.json({ favs: favs[0]?.favs || 0 });
 }
 
 export async function POST(request: ExpoRequest) {
   const id = request.expoUrl.pathname.split('/')[3];
-  if (!favs[id]) {
-    favs[id] = 1;
+  const favs = await Favs.findAll({
+    where: {
+      id
+    }
+  });
+  let myFav;
+  if (favs.length) {
+    myFav = favs[0];
+    myFav.favs++;
+    await myFav.save();
   } else {
-    favs[id] += 1;
+    myFav = await (new Favs({ id, favs: 1 })).save();
   }
-  console.log(favs)
-  return ExpoResponse.json({ favs: favs[id] });
+  return ExpoResponse.json({ favs: myFav.favs });
 }
