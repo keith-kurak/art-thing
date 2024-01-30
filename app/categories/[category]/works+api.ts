c5dd4a8737749f8f8b2eb52ec241ced0ff6f14d1
Keith Kurak <keith.kurak@gmail.com>
2024-01-30 11:44:47 -0500

    use local data for Basketry

--- app/categories/[category]/works+api.ts
+++ app/categories/[category]/works+api.ts
@@ -2,6 +2,9 @@ import { ExpoRequest, ExpoResponse } from "expo-router/server";
 
 export async function GET(request: ExpoRequest) {
   const category = request.expoUrl.searchParams.get('category')
+  if (category === 'Basketry') {
+    return ExpoResponse.json(require('@/local-data/Basketry.json'))
+  }
   const response = await fetch(
     `https://openaccess-api.clevelandart.org/api/artworks?has_image=1&type=${category}&limit=20&fields=id,title,images,creation_date_earliest,creation_date_latest`
   );
--- app/works/[id]/details+api.ts
+++ app/works/[id]/details+api.ts
@@ -2,6 +2,11 @@ import { ExpoRequest, ExpoResponse } from "expo-router/server";
 
 export async function GET(request: ExpoRequest) {
   const id = request.expoUrl.searchParams.get('id')
+  const localData = require('@/local-data/Basketry.json');
+  const localRecord = localData.data.find((record: any) => record.id == id);
+  if (localRecord) {
+    return ExpoResponse.json({ data: localRecord })
+  }
   const response = await fetch(
     `https://openaccess-api.clevelandart.org/api/artworks/${id}`
   );
